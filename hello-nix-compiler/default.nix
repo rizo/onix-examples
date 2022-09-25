0d39a973c2c1aa7a60bd88c05e3d01555280dc75
Rizo I <rizo@odis.io>
2022-09-25 23:18:23 +0100

    Minor changes to hello-nix-compiler.

--- hello-nix-compiler/default.nix
+++ hello-nix-compiler/default.nix
@@ -4,14 +4,11 @@ let
   ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14;
   onix = import ./nix/onix.nix { inherit pkgs ocamlPackages; };
 in {
-  packages = onix.build {
-    lockFile = ./onix-lock.nix;
-  };
+  packages = onix.build { lockFile = ./onix-lock.nix; };
 
   lock = onix.lock {
-    repoUrl = "https://github.com/ocaml/opam-repository.git#52c72e08d7782967837955f1c50c330a6131721f";
-    resolutions = {
-      ocaml-system = "4.14.0";
-    };
+    repoUrl =
+      "https://github.com/ocaml/opam-repository.git#52c72e08d7782967837955f1c50c330a6131721f";
+    resolutions = { ocaml-system = "4.14.0"; };
   };
 }
