8c75f00cd51613cbbc857293bbb7eb722bd853b3
Rizo I <rizo@odis.io>
2022-10-08 22:08:06 +0100

    Update examples.

--- hello-nix-compiler/default.nix
+++ hello-nix-compiler/default.nix
@@ -3,7 +3,7 @@
 let
   ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14;
   onix = import ./nix/onix.nix { inherit pkgs ocamlPackages; };
-in {
+in rec {
   scope = onix.build { lockFile = ./onix-lock.nix; };
 
   lock = onix.lock {
@@ -11,5 +11,6 @@ in {
       "https://github.com/ocaml/opam-repository.git#f3dcd527e82e83facb92cd2727651938cb9fecf9";
     resolutions = { "ocaml-system" = "*"; };
   };
-}
 
+  shell = pkgs.mkShell { inputsFrom = [ scope.hello-nix-compiler ]; };
+}
--- universe/default.nix
+++ universe/default.nix
@@ -2,11 +2,12 @@
 
 let
   ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14;
-
-  onix = import ./../../default.nix { inherit pkgs ocamlPackages; };
-
-  scope = onix.build {
-    ocaml = ocamlPackages.ocaml;
-    lock = ./onix-lock.nix;
+  onix = import ./nix/onix.nix { inherit pkgs ocamlPackages; };
+in rec {
+  scope = onix.build { lockFile = ./onix-lock.nix; };
+  lock = onix.lock {
+    repoUrl =
+      "https://github.com/ocaml/opam-repository.git#f3dcd527e82e83facb92cd2727651938cb9fecf9";
+    resolutions = { "ocaml-system" = "*"; };
   };
-in scope.universe
+}
