6bc7674a71c8e6a1eed037f63e7077d12d560159
Rizo I <rizo@odis.io>
2022-11-24 23:11:56 +0000

    Fix more examples.

--- with-pin-depends/default.nix
+++ with-pin-depends/default.nix
@@ -2,12 +2,17 @@
 
 let
   ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14;
-  onix = import ./nix/onix.nix { inherit pkgs ocamlPackages; };
-in {
-  scope = onix.build { lockFile = ./onix-lock.nix; };
-  lock = onix.lock {
-    repoUrl =
-      "https://github.com/ocaml/opam-repository.git#52c72e08d7782967837955f1c50c330a6131721f";
-    resolutions = { "ocaml-system" = "*"; };
-  };
+
+  # Use this in your code:
+  # onix = import (builtins.fetchGit {
+  #   url = "https://github.com/odis-labs/onix.git";
+  #   rev = "4960c6bb9ba3b8cec1d20b35b290350b1555d188";
+  # }) { inherit pkgs ocamlPackages; };
+  onix = import ./../onix.nix { inherit pkgs ocamlPackages; };
+
+in onix.project ./. {
+  repositories = [
+    "https://github.com/ocaml/opam-repository.git#03cdcda5b3a74772bd5f92ff9bcfb1b1310ceaf3"
+  ];
+  resolutions = { "ocaml-system" = "*"; };
 }
