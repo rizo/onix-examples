e0141d0f2906a6e208aa7d82feeb7919e6a1e8ce
Rizo I <rizo@odis.io>
2022-11-24 23:00:42 +0000

    Fix hello-with-overrides.

--- hello-with-overrides/default.nix
+++ hello-with-overrides/default.nix
@@ -2,19 +2,14 @@
 
 let
   ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14;
-  onix = import ./nix/onix.nix { inherit pkgs ocamlPackages; };
+  onix = import ./../onix.nix { inherit pkgs ocamlPackages; };
 
-in {
-  scope = onix.build {
-    lockFile = ./onix-lock.nix;
-    overrides = self: super: {
-      dune = pkgs.dune_3;
-      ocaml-base-compiler = ocamlPackages.ocaml;
-    };
-  };
-
-  lock = onix.lock {
-    repoUrl =
-      "https://github.com/ocaml/opam-repository.git#52c72e08d7782967837955f1c50c330a6131721f";
+in onix.project ./. {
+  repositories = [
+    "https://github.com/ocaml/opam-repository.git#52c72e08d7782967837955f1c50c330a6131721f"
+  ];
+  overrides = self: super: {
+    dune = pkgs.dune_3;
+    ocaml-base-compiler = ocamlPackages.ocaml;
   };
 }
