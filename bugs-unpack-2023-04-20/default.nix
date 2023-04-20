440f9598cd654e0aefb620326a2ef1bde0d8da1c
Rizo I <rizo@odis.io>
2023-04-20 18:43:59 +0100

    unpacker produced multiple directories

---
+++ bugs-unpack-2023-04-20/default.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  onix = import (builtins.fetchGit {
+    url = "https://github.com/odis-labs/onix.git";
+    rev = "a5534a86d3eee96c80c41c3c895407fef06dcc9a";
+  }) {
+    inherit pkgs;
+    verbosity = "info";
+  };
+
+in onix.env {
+  path = ./.;
+  deps = { "ocaml-system" = "*"; };
+}
